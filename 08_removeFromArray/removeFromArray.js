af15ca8d11a2c59c159ddd71777115783836d181
p1zzakat <rbanine@gmail.com>
2025-07-03 09:18:42 +0100

    solution

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr) {
+  const args = [...arguments].slice(1);
+  for (let element of args) {
+    while (arr.includes(element)) {
+        arr.splice(arr.indexOf(element), 1);
+    }
+  }
+  return arr;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
