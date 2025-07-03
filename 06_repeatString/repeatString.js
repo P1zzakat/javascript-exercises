9d03316d8cf1a722a424072814d2283d103b820f
p1zzakat <rbanine@gmail.com>
2025-07-03 08:47:10 +0100

    Solution

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(str, iterations) {
+  let result = '';
+  if (iterations < 0) return 'ERROR';
+  for (let i = 0; i < iterations; i++) result += str;
+  return result;
 };
 
 // Do not edit below this line
