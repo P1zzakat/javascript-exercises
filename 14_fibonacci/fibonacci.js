debf4be72e2c4debe652b96a7a557f588b15cf15
p1zzakat <rbanine@gmail.com>
2025-07-04 13:14:24 +0100

    solution

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
+    // Make sure input is correct
+    if (n < 0) return 'OOPS';
+    if (!n) return 0;
+    // First elements of the sequence
+    let fibonacciArray = [1, 1];
+    for (let i = fibonacciArray.length; fibonacciArray.length <= n; i++) {
+        const nextElement = fibonacciArray[i - 2] + fibonacciArray [i - 1];
+        fibonacciArray.push(nextElement);
+    }
 
+    return fibonacciArray[n - 1];
 };
 
 // Do not edit below this line
