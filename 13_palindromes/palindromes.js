4d133b1899ed4688964c7e768f1de17dcb6dbf9a
p1zzakat <rbanine@gmail.com>
2025-07-04 12:56:38 +0100

    solution

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let splitStr = str.toLowerCase().split('');
+    let result = new Array();
+    splitStr.forEach(element => {
+        if (element !== ' ' && element !== '!' && element !== ',' && element !== '.') result.push(element);
+    });
+    for (let i = 0; i < result.length; i++) {
+        if (result[i] !== result.reverse()[i]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
